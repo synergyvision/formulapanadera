54f970afea767a7d7c19ea62d57ed6ef8c069abb
Alba Sofía <assanchez.17@est.ucab.edu.ve>
2020-10-15 17:59:47 -0400

    fix004: Added manipulation times to formula details

--- formulapanadera/src/app/shared/components/formula/formula-step/formula-step.component.ts
+++ formulapanadera/src/app/shared/components/formula/formula-step/formula-step.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from "@angular/core";
 import { ASSETS } from 'src/app/config/assets';
+import { MANIPULATION_STEP } from 'src/app/config/formula';
 import { ICONS } from "src/app/config/icons";
 import { StepDetailsModel } from "src/app/core/models/formula.model";
 import { FormatNumberService } from "src/app/core/services/format-number.service";
@@ -25,5 +26,9 @@ export class FormulaStepComponent {
   fahrenheitTemperature(value) {
     value = this.formatNumberService.fromCelsiusToFahrenheit(value);
     return value;
+  }
+
+  isManipulationStep(): boolean {
+    return this.step.number == MANIPULATION_STEP-1
   }
 }
