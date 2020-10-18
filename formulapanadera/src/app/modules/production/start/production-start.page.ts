ff71fe9530a6fc85093c19cebb6ceb17663143a9
Alba Sofía <assanchez.17@est.ucab.edu.ve>
2020-10-18 18:03:57 -0400

    feature009: Bug fixes, changes on i18n files and fixed mixing order changes

--- formulapanadera/src/app/modules/production/start/production-start.page.ts
+++ formulapanadera/src/app/modules/production/start/production-start.page.ts
@@ -224,4 +224,27 @@ export class ProductionStartPage implements OnInit {
     });
     await alert.present();
   }
+
+  async stopProductionAlert() {
+    const alert = await this.alertController.create({
+      header: this.languageService.getTerm("production.warning.name"),
+      message: this.languageService.getTerm("production.warning.stop"),
+      cssClass: "confirm-alert",
+      buttons: [
+        {
+          text: this.languageService.getTerm("action.cancel"),
+          role: "cancel",
+          handler: () => {
+            this.changeProcessStatus();
+          },
+        },
+        {
+          text: this.languageService.getTerm("action.ok"),
+          cssClass: "confirm-alert-accept",
+          handler: () => {},
+        },
+      ],
+    });
+    await alert.present();
+  }
 }
